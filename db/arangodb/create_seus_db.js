171fed7e45ee66cd20b613a50bdd1ee2108650d6
Fernando Luz <prof.fernando.luz@gmail.com>
2024-01-22 15:40:28 +0000

    build(docker): add curl into arangodb to validate if the arandodb is alive in docker

--- db/arangodb/create_seus_db.js
+++ db/arangodb/create_seus_db.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const db = require('@arangodb').db;
-const databaseName = 'seus';
+const databaseName = process.env.SEUS_DB_NAME;
 
 // Create a new database (remove if the previous exists)
 if (db._databases().includes(databaseName)) {
     db._dropDatabase(databaseName);
 }
+
 db._createDatabase(
     databaseName, 
     {},
